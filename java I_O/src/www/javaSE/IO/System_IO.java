bdf5e6731a652a188a651a612f9ab839f184c85b
IQQcode <2726109782@qq.com>
2019-04-06 22:02:09 +0800

    System_IO

---
+++ java I_O/src/www/javaSE/IO/System_IO.java
@@ -0,0 +1,50 @@
+package www.javaSE.IO;
+
+import  java.io.*;
+import  java.io.IOException;
+import  java.util.Scanner;
+
+/**
+ *  使用System.out为 OutputStream实例化
+ */
+/*
+public class System_IO {
+    public static void main(String[] args) throws  Exception {
+        OutputStream out = System.out ;
+        out.write("使用System.out为OutputStream实例化".getBytes());
+    }
+}
+*/
+
+/**
+ * 系统输入：
+ * System.in 是InputStream的直接对象
+ */
+/*
+public class System_IO {
+    public static void main(String[] args) throws  Exception {
+        InputStream in = System.in;
+        System.out.println("亲输入内容: ");
+        //字节数组存放输入的字符串
+        byte[] data = new byte[1024];
+        int len = 0;
+        len = in.read(data);
+        System.out.println("输入内容为：" + new String(data,0,len));
+    }
+}
+*/
+
+/**
+ * 自定义分隔符为输入的结束标志
+ */
+
+public class System_IO {
+    public static void main(String[] args) throws  Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入内容：");
+        input.useDelimiter("-");
+        if(input.hasNext()) {
+            System.out.println("输入内容为：" + input.next());
+        }
+    }
+}
