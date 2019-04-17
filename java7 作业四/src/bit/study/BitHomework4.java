e5c1acb3f64e9d6231e4b768470bc61ef2232b27
IQQcode <2726109782@qq.com>
2019-04-17 22:25:26 +0800

    add Homework bir-4

---
+++ java7 作业四/src/bit/study/BitHomework4.java
@@ -0,0 +1,27 @@
+package bit.study;
+
+import java.util.Scanner;
+
+//完数
+/*
+public class BitHomework4 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 1000; i++) {
+            int sum = 0;
+            for(int num = 1; num <= i/2; num++) {
+                if(i % num ==0) {
+                    sum += num;
+                }
+            }
+            if(i == sum) {
+                System.out.print(i + "、");
+            }
+        }
+    }
+}
+*/
+
+
+
+
+
