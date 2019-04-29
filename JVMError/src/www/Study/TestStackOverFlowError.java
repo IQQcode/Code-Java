4028c10385c0b2f4e3b7817268a36e5284e6c423
IQQcode <2726109782@qq.com>
2019-04-29 22:56:59 +0800

    add code

--- JVMError/src/www/Study/TestStackOverFlowError.java
+++ JVMError/src/www/Study/TestStackOverFlowError.java
@@ -1,10 +1,13 @@
 package www.Study;
+import  java.util.Scanner;
 
 /**
  * 递归无出口 StackOverFlowError
  *
  * JVM参数 -Xss108k
  */
+
+/*
 
 public class TestStackOverFlowError {
     private int stackLength = 1;
@@ -22,3 +25,32 @@ public class TestStackOverFlowError {
         }
     }
 }
+*/
+
+
+/**
+ * Fibonacci 栈溢出
+ *
+ * 递归无出口
+ */
+
+public class TestStackOverFlowError {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the num: ");
+        int num = input.nextInt();
+        System.out.print("The result is : " +  Fibonacci(num));
+    }
+
+    public static int Fibonacci(int num) {
+        /*if(num <= 2) {
+            return 1;
+        }else {
+            return Fibonacci(num-1) + Fibonacci(num-2);
+        }*/
+
+        //栈溢出
+        return Fibonacci(num-1) + Fibonacci(num-2);
+    }
+}
+
