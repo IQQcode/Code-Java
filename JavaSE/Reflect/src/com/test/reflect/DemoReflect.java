560cdd73f472e5889df486e59b6857b3b6dac541
Mr.Q <2726109782@qq.com>
2019-07-16 00:04:38 +0800

    add code

--- JavaSE/Reflect/src/com/test/reflect/DemoReflect.java
+++ JavaSE/Reflect/src/com/test/reflect/DemoReflect.java
@@ -21,6 +21,14 @@ public class DemoReflect {
             System.out.println(fields.length);
 
             //通过反射API调用构造方法
+
+            /**
+             * newInstance()也是用来创建新的对象,其与new()的区别是:
+             *
+             * newInstance():弱类型,效率低,只能调用无参构造
+             *
+             * new():强类型,高效率,能调用任何public构造器
+             */
             User u = cls2.newInstance();
             System.out.println(u);
             //指定构造器
@@ -30,6 +38,7 @@ public class DemoReflect {
 
             //通过反射API来调用普通方法
             //----> 正向调用   u3.setName("IQQCode")
+
             /**
              * 反射的好处是只传入方法名，传入的方法和参数都是变量，就可以动态的调用相关的方法
              */
@@ -41,7 +50,9 @@ public class DemoReflect {
             //通过反射API操作属性
             User u4 = cls2.newInstance();
             Field field = cls2.getDeclaredField("name");
-            field.set(u4,"犹梦");
+            field.setAccessible(true);  //直接访问到私有属性
+            field.set(u4,"犹梦");  //通过反射直接写属性
+            System.out.println(field.get(u4));  //通过反射直接读属性
         } catch (Exception e) {
             e.printStackTrace();
         }
