baf3100612db6266c6a02645ecfe0628225d1d18
Mr.Q <2726109782@qq.com>
2019-07-20 22:59:21 +0800

    alter code

--- JavaSE/MultiThread/src/com/Synchronized/Synchronized_A.java
+++ JavaSE/MultiThread/src/com/Synchronized/Synchronized_A.java
@@ -1,98 +1,44 @@
 package com.Synchronized;
 
 /**
- *同步问题
+ * synchronized说明：
+ *
+ * 1.synchronized(this) { } 锁同一个对象;
+ *
+ * 2.synchronized(sync.class) { } 全局锁，锁类对应的Class对象;
+ *
+ * 3. ststic synchronized相当于全局锁;
  */
-
-/*class MythreadA implements Runnable {
-    private Integer tickets = 10;
-
-    @Override
-    public void run() {
-        while(tickets > 0) {
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() +
-                    "还有" + this.tickets-- + "张票");
-        }
-    }
-}*/
-
 
 /**
- *  synchronized()处理同步问题
- *
- *  实现这把"锁"的功能，可以采用关键字synchronized来处理
- *
- *  使用synchronized关键字处理有两种模式：同步代码块、同步方法
+ * @static 全局锁
  */
-
-//同步代码块
-
-/*
-class MythreadA implements Runnable {
-    private Integer tickets = 10;
-
-    @Override
-    public void run() {
-        while(tickets > 0) {
-            synchronized (this) {
-                if(tickets > 0) {
-
-                    //在此同步代码块中，只有一个线程在跑
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(Thread.currentThread().getName() +
-                            "还有" + this.tickets-- + "张票");
-                }
-            }
+class Sync {
+    //锁的是 Sync的对象
+    public static synchronized void test() {
+        System.out.println("test()方法开始，当前线程为： " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
-}
-*/
-
-//同步方法
-
-class MythreadA implements Runnable {
-    private Integer tickets = 10;
-
-    @Override
-    public void run() {
-        while(tickets > 0) {
-            this.sale();
-        }
-    }
-    //隐式锁对象this
-    public synchronized void sale() {
-        if(this.tickets > 0) {
-
-            //在此同步代码块中，只有一个线程在跑
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() +
-                    "还有" + this.tickets-- + "张票");
-        }
+        System.out.println("test()方法结束，当前线程为： " + Thread.currentThread().getName());
     }
 }
 
+class Mythread implements Runnable {
+    @Override
+    public void run() {
+        Sync sync = new Sync();
+        sync.test();
+    }
+}
 
 public class Synchronized_A {
     public static void main(String[] args) {
-        MythreadA mythreadA = new MythreadA();
-        Thread thread1 = new Thread(mythreadA,"黄牛A");
-        Thread thread2 = new Thread(mythreadA,"黄牛B");
-        Thread thread3 = new Thread(mythreadA,"黄牛C");
-        thread1.start();
-        thread2.start();
-        thread3.start();
+            Mythread mythread = new Mythread();
+            new Thread(mythread,"A").start();
+            new Thread(mythread,"B").start();
+            new Thread(mythread,"C").start();
     }
 }
--- JavaSE/MultiThread/src/com/Synchronized/Synchronized_B.java
+++ JavaSE/MultiThread/src/com/Synchronized/Synchronized_B.java
@@ -1,17 +1,15 @@
 package com.Synchronized;
 
+
 /**
- * synchronized说明：
- *
- * 1.synchronized(this) { } 锁同一个对象;
- *
- * 2.synchronized(sync.class) { } 全局锁，锁类对应的Class对象;
- *
- * 3. ststic synchronized相当于全局锁;
+ * @Author: Mr.Q
+ * @Date: 2019-07-20 10:36
+ * @Description:
  */
 
-class Sync {
-    public  static synchronized void test() {
+class SyncB {
+    //锁的是 Sync的对象
+    public  synchronized void test() {
         System.out.println("test()方法开始，当前线程为： " + Thread.currentThread().getName());
         try {
             Thread.sleep(4000);
@@ -21,19 +19,20 @@ class Sync {
         System.out.println("test()方法结束，当前线程为： " + Thread.currentThread().getName());
     }
 }
-class MythreaB implements Runnable {
+
+class MythreadB implements Runnable {
+    SyncB sync = new SyncB();
     @Override
     public void run() {
-        Sync sync = new Sync();
         sync.test();
     }
 }
 
 public class Synchronized_B {
     public static void main(String[] args) {
-            MythreaB mythreaB = new MythreaB();
-            new Thread(mythreaB,"黄牛A").start();
-            new Thread(mythreaB,"黄牛B").start();
-            new Thread(mythreaB,"黄牛C").start();
+        MythreadB mythreadB = new MythreadB();
+        new Thread(mythreadB,"A").start();
+        new Thread(mythreadB,"B").start();
+        new Thread(mythreadB,"C").start();
     }
 }
