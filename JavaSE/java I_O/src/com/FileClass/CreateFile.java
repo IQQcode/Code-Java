c4f27d7a3b509d550a5922f7773b416a1a354b2b
Mr.Q <2726109782@qq.com>
2019-08-13 20:13:39 +0800

    Stream

--- JavaSE/java I_O/src/com/FileClass/CreateFile.java
+++ JavaSE/java I_O/src/com/FileClass/CreateFile.java
@@ -13,17 +13,20 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String[] args) {
         //1.取得File对象
-        File file = new File(File.separator + "G:" + File.separator +
-                "javaTest" + File.separator + "www" + File.separator + "javaSE.IO" + File.separator + "Hellow!");
+        File file = new File(File.separator + "C:" + File.separator +
+                "Users" + File.separator + "j2726" + File.separator + "Desktop" + File.separator +"javaIO" + File.separator + "Mr.Q" + File.separator +"File" + File.separator + "Hello!");
 
         //2.判断父路径是否存在，不存在则创建多级父路径
         if(!file.getParentFile().exists()) {
-            file.getParentFile().mkdirs();    //多级父路径 + mkdirs();
+            //多级父路径 + mkdirs()创建文件
+            file.getParentFile().mkdirs();
+            // 如果直接 file.mkdirs()则会创建为文件夹
         }
 
         //3.判断文件是否存在，不存在则创建文件
         if(file.exists()) {
-            System.out.println("文件已存在！");
+            System.err.println("文件已存在！删除后重新创建...");
+            file.delete();
         }else {
             try {
                 file.createNewFile();
@@ -34,7 +37,4 @@ public class CreateFile {
         }
     }
 }
-
-
-
 
