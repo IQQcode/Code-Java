3bd234707adbebaad4871e2b91589b42ea33ac27
Mr.Q <2726109782@qq.com>
2019-10-12 23:26:10 +0800

    properties test

---
+++ SQL/JDBC/JDBCBasicUniteTest/src/main/java/com/jdcb/basic/JDBCBasicDemo.java
@@ -0,0 +1,36 @@
+package com.jdcb.basic;
+
+import java.sql.*;
+
+/**
+ * @Author: Mr.Q
+ * @Date: 2019-10-08 16:05
+ * @Description:jdbc连接数据库基础操作
+ */
+public class JDBCBasicDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 1.导入驱动jar包
+        // 2.注册驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 3.获取数据库连接
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","1234");
+        // 4.定义SQL语句
+        String sql = "select * from student";
+        // 5.获取执行SQL的对象 Statement
+        Statement statement = connection.createStatement();
+        // 6.执行SQL
+        ResultSet resultSet = statement.executeQuery(sql);
+        //遍历结果集
+        while (resultSet.next()) {
+            //每一行记录认为是一个对象
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            int age = resultSet.getInt("age");
+            String major = resultSet.getString("major");
+            System.out.println("id : "+id+" \nname : "+name +"\nage : "+age +"\nmajor : "+major);
+            System.out.println();
+        }
+        connection.close();
+        statement.close();
+    }
+}
---
+++ SQL/JDBC/JDBCBasicUniteTest/src/main/java/com/jdcb/basic/JDBCProperties.java
@@ -0,0 +1,52 @@
+package com.jdcb.basic;
+
+import java.sql.*;
+
+/**
+ * @Author: Mr.Q
+ * @Date: 2019-10-11 19:33
+ * @Description:
+ */
+public class JDBCProperties {
+    public static void main(String[] args) throws ClassNotFoundException {
+        // 1.加载驱动
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2. 获取连接
+        String URL = "jdbc:mysql://localhost:3306/test";
+        String USERNAME = "root";
+        String PASSWORD = "1234";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME,PASSWORD);
+
+            String sql = "select * from student";
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery(sql);
+            //遍历结果集
+            while (resultSet.next()) {
+                //每一行记录认为是一个对象
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                String major = resultSet.getString("major");
+                System.out.println("id : "+id+" \nname : "+name +"\nage : "+age +"\nmajor : "+major);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
