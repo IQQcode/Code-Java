ce2327f09c3336f94fc7bc4af6c88e542c664c71
Mr.Q <2726109782@qq.com>
2020-03-15 23:36:07 +0800

    ajax

---
+++ JavaWeb/AJAX/AjaxLearing/src/com/iqqcode/ajax/servlet/AjaxJsServlet.java
@@ -0,0 +1,39 @@
+package com.iqqcode.ajax.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Author: Mr.Q
+ * @Date: 2020-03-15 10:19
+ * @Description:原生js实现
+ */
+@WebServlet("/ajaxServlet")
+public class AjaxJsServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.获取请求参数
+        String username = request.getParameter("username");
+        //处理业务逻辑，耗时
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //2.打印username
+        System.out.println(username);
+
+        //3.响应
+        response.getWriter().write("hello : " + username);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
