adf3a2ec36b96222c514fd024a1ffafa6d738304
iqqcode <2726109782@qq.com>
2020-08-01 11:35:40 +0800

    spring initializr

--- c-Java EE/2-SpringMVC/SpringMVC-Demo/springmvc01_mvc/src/main/java/com/iqqcode/service/impl/UserServiceImpl.java
+++ c-Java EE/2-SpringMVC/SpringMVC-Demo/springmvc01_mvc/src/main/java/com/iqqcode/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.iqqcode.service.impl;
 
 import com.iqqcode.dao.UserDao;
+import com.iqqcode.dao.impl.UserDaoImpl;
 import com.iqqcode.service.UserService;
 import lombok.Data;
 
@@ -9,8 +10,14 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
+    public void setUserDao(UserDaoImpl userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public void login() {
         userDao.login();
     }
+
+
 }
---
+++ c-Java EE/4-SpringBoot/SpringBoot_contact/hellospring_initializr/src/main/java/com/example/hellospring/HellospringApplication.java
@@ -0,0 +1,16 @@
+package com.example.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr.Q
+ */
+@SpringBootApplication
+public class HellospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellospringApplication.class, args);
+    }
+
+}
